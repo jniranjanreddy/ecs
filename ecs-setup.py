4a8e90b5f6fcab9c9db7164b8e74ab62ebd6bbfb
niru <you@example.com>
2021-10-12 21:03:10 -0400

    ko

--- ecs-setup.py
+++ ecs-setup.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
-import sys
 import os
+import sys
+import platform
+
 ARGUMENT = sys.argv[1]
 if ARGUMENT == "apply":
     infra = ("aws cloudformation create-stack \
         --capabilities CAPABILITY_IAM \
         --stack-name ecs-core-infrastructure \
         --template-body file://./core-infrastructure-setup.yml")
+    platform.os.system("uname -a")
 elif ARGUMENT == "destroy":
     print("!! Sit back and Relax ECS Cluster is being Destrpyed !!")
 else:
-    print("The usage of script:" sys.argv[1] + "apply/destroy")
+    print("The usage of script: " + sys.argv[0] + " apply/destroy")
