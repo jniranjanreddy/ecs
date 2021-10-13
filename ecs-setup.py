45b552cd478cccc88a46f91236e1e0ad71932895
niru <you@example.com>
2021-10-12 21:22:46 -0400

    lk

--- ecs-setup.py
+++ ecs-setup.py
@@ -2,15 +2,25 @@
 import os
 import sys
 import platform
+def Clear():
+    cls = ("clear")
+    os.system(cls)
+Clear()
 
 ARGUMENT = sys.argv[1]
 if ARGUMENT == "apply":
+    Clear()
+    print("Sit back and Relax ECS Cluster is being Deployed !!")
     infra = ("aws cloudformation create-stack \
         --capabilities CAPABILITY_IAM \
         --stack-name ecs-core-infrastructure \
         --template-body file://./core-infrastructure-setup.yml")
-    platform.os.system("uname -a")
+    platform.os.system(infra)
 elif ARGUMENT == "destroy":
-    print("!! Sit back and Relax ECS Cluster is being Destrpyed !!")
+    Clear()
+    print("!! Sit back and Relax ECS Cluster is being Destroyed !!")
+    delete = ("aws cloudformation delete-stack --stack-name ecs-core-infrastructure")
+    platform.os.system(delete)
+    
 else:
     print("The usage of script: " + sys.argv[0] + " apply/destroy")
